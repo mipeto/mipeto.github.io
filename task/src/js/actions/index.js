2a7806698728d629982ace6d6389761dfe8ae9ab
Abdullov Ildar <abdullov.i@outlook.com>
2020-03-23 16:56:46 +0400

    clear

--- task/src/js/actions/index.js
+++
@@ -1,3 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const changePage = createAction('CHANGE_PAGE');
--- task/src/js/components/Penthouses.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {BrowserRouter} from 'react-router-dom';
-
-export default class Penthous extends React.Component{
-    render () {
-        return (<section className='penthous'>
-        </section>)
-    }
-}
